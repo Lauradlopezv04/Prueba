38c1b3fdccf356674dc1439382a9c928c06dd5db
Laura <laurad.lopez0401@gmail.com>
2021-05-24 15:32:54 -0500

    hola

---
+++ ejercicio_3.py
@@ -0,0 +1,15 @@
+# Autor: EasyCode By Laura 
+# Fecha: 18/04/2021
+# Version: 1.0
+# Nombre del proyecto: Ehercicio #1
+# Correo: laurad.lopez0401@gmail.com
+
+numero_uno = float(input("Digite un primer valor: "))
+numero_dos = float(input("Digite un segundo valor: "))
+numero_tres = float(input("Digite un tercer valor: "))
+
+promedio = float((numero_uno+numero_dos+numero_tres)/3)
+
+print("El promedio es: ", promedio)
+print("Terminado")
+
